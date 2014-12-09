8aa909875063360f8f7f7e5f7cf807bd44f58963
tmiyamon <miyamototakuya@gmail.com>
2014-12-09 23:44:26 +0900

    implement recycler view in app

--- app/src/main/java/com/tmiyamon/androidsampler/MainActivity.java
+++ app/src/main/java/com/tmiyamon/androidsampler/MainActivity.java
@@ -3,11 +3,14 @@ package com.tmiyamon.androidsampler;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,22 +18,41 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 
 public class MainActivity extends ActionBarActivity {
+    public static final String ASSET_FILE_NAME = "fragment";
+    public static final String KEY_FRAGEMENT = "fragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, buildFragment())
                     .commit();
         }
+    }
 
-        List<String> fragments = new ArrayList<>();
+    public Fragment buildFragment() {
+        ArrayList<String> fragmentClassNames = getFragmentClassNames(ASSET_FILE_NAME);
+
+        Fragment fragment = new PlaceholderFragment();
+        Bundle bundle = new Bundle();
+        bundle.putStringArrayList(KEY_FRAGEMENT, fragmentClassNames);
+        fragment.setArguments(bundle);
+
+        return fragment;
+    }
+
+    public ArrayList<String> getFragmentClassNames(String filename) {
+        ArrayList<String> fragments = new ArrayList<>();
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(getResources().getAssets().open("fragments")));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(getResources().getAssets().open(filename)));
             String line;
             while((line = reader.readLine()) != null) {
                 fragments.add(line);
@@ -39,6 +61,8 @@ public class MainActivity extends ActionBarActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return fragments;
     }
 
 
@@ -69,6 +93,9 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        @InjectView(R.id.recyclerView)
+        RecyclerView recyclerView;
+
         public PlaceholderFragment() {
         }
 
@@ -76,7 +103,70 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            ButterKnife.inject(this, rootView);
+
+            initRecyclerView(getArguments().getStringArrayList(KEY_FRAGEMENT));
+
             return rootView;
+        }
+
+        public void initRecyclerView(List<String> items) {
+
+            List<ListItem> listItems = new ArrayList<>(items.size());
+            for (String fragmentClassName : items) {
+                try {
+                    listItems.add(new ListItem(fragmentClassName));
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            recyclerView.setAdapter(new Adapter(listItems));
+            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            @InjectView(R.id.textView)
+            TextView textView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                ButterKnife.inject(this, itemView);
+            }
+        }
+
+        public class Adapter extends RecyclerView.Adapter<ViewHolder> {
+            List<ListItem> items;
+
+            public Adapter(List<ListItem> items) {
+                this.items = items;
+            }
+
+            @Override
+            public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_main, parent, false);
+                return new ViewHolder(itemView);
+            }
+
+            @Override
+            public void onBindViewHolder(ViewHolder holder, int position) {
+                holder.textView.setText(items.get(position).text);
+            }
+
+            @Override
+            public int getItemCount() {
+                return items.size();
+            }
+        }
+
+        public class ListItem {
+            String text;
+            Class<?> fragmentClass;
+
+            public ListItem(String fragmentClassName) throws ClassNotFoundException {
+                this.text = fragmentClassName;
+                this.fragmentClass = Class.forName(fragmentClassName);
+            }
         }
     }
 }
